5793290113622f59db4766d395fea717918f6688
Szymon Zimny <szymcioo96@interia.pl>
2018-06-26 21:01:49 +0200

    dodanie metod do podgłaśniania, ściszania i wyłączenia dźwięku

--- Mp3Player/Form1.cs
+++ Mp3Player/Form1.cs
@@ -7,14 +7,41 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Runtime.InteropServices;
 
 namespace Mp3Player
 {
     public partial class Form1 : Form
     {
+        private const int APPCOMMAND_VOLUME_MUTE = 0x80000;
+        private const int APPCOMMAND_VOLUME_UP = 0xA0000;
+        private const int APPCOMMAND_VOLUME_DOWN = 0x90000;
+        private const int WM_APPCOMMAND = 0x319;
+        [DllImport("user32.dll")]
+        public static extern IntPtr SendMessageW(IntPtr hWnd, int Msg,
+            IntPtr wParam, IntPtr lParam);
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Mute()
+        {
+            SendMessageW(this.Handle, WM_APPCOMMAND, this.Handle,
+               (IntPtr)APPCOMMAND_VOLUME_MUTE);
+        }
+
+        private void VolDown()
+        {
+            SendMessageW(this.Handle, WM_APPCOMMAND, this.Handle,
+               (IntPtr)APPCOMMAND_VOLUME_DOWN);
+        }
+
+        private void VolUp()
+        {
+            SendMessageW(this.Handle, WM_APPCOMMAND, this.Handle,
+               (IntPtr)APPCOMMAND_VOLUME_UP);
         }
     }
 }
